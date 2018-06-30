d3570ef69530256d3b8644e0a8a7fba1b802f7e5
Nathan Sosnovske <nsosnov@microsoft.com>
2018-06-30 13:14:51 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,3 @@
 start-process -FilePath "c:\program files\unity\editor\unity.exe" -ArgumentList "-nographics","-projectPath","$PSScriptRoot\src","-logFile","unity.log","-batchMode","-buildWindows64Player","$PSScriptRoot\output\Endless.exe","-quit" -Wait -NoNewWindow
+
+get-content unity.log
