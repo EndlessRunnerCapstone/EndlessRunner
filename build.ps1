be167be3c88ed4f8e953b11c16b6fce1f018b2ef
Nathan Sosnovske <nathan@Nathans-MacBook-Pro.local>
2018-07-04 16:35:10 -0700

    Update

--- build.ps1
+++ build.ps1
@@ -8,7 +8,7 @@ if($proc.ExitCode -gt 0)
   exit $proc.ExitCode
 }
 
-$proc = start-process -FilePath "c:\program files\unity\editor\unity.exe" -ArgumentList "-nographics","-projectPath","$PSScriptRoot\src","-logFile","unityGL.log","-batchMode","-executeMethod,"WebGLBuilder.build","-quit" -Wait -NoNewWindow
+$proc = start-process -FilePath "c:\program files\unity\editor\unity.exe" -ArgumentList "-nographics","-projectPath","$PSScriptRoot\src","-logFile","unityGL.log","-batchMode","-executeMethod","WebGLBuilder.build","-quit" -Wait -NoNewWindow
 
 get-content unityGL.log
 
