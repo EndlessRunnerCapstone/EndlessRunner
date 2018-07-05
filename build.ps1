8f2691e577c935fc6095cb97a24d28afd24dc4bc
Nathan Sosnovske <nathan@Nathans-MacBook-Pro.local>
2018-07-04 17:43:39 -0700

    Bump

--- build.ps1
+++ build.ps1
@@ -1,6 +1,5 @@
 $proc = start-process -FilePath "c:\program files\unity\editor\unity.exe" -ArgumentList "-nographics","-projectPath","$PSScriptRoot\src","-logFile","unity.log","-batchMode","-buildWindows64Player","$PSScriptRoot\output\Endless.exe","-quit" -Wait -NoNewWindow
 
-
 get-content unity.log
 
 if($proc.ExitCode -gt 0)
