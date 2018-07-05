bb8e3bc0f3fe1550b9c5fcc5ed34bed4fda11707
Nathan Sosnovske <nathan@Nathans-MacBook-Pro.local>
2018-07-04 17:26:12 -0700

    Bump

--- build.ps1
+++ build.ps1
@@ -7,7 +7,6 @@ if($proc.ExitCode -gt 0)
   exit $proc.ExitCode
 }
 
-
 $proc = start-process -FilePath "c:\program files\unity\editor\unity.exe" -ArgumentList "-nographics","-projectPath","$PSScriptRoot\src","-logFile","unityGL.log","-batchMode","-executeMethod","WebGLBuilder.build","-quit" -Wait -NoNewWindow
 
 get-content unityGL.log
