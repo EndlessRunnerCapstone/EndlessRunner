16cf5eac1c01474077361c68431f1be3eda8976f
Nathan Sosnovske <nathan@sosnovske.com>
2018-07-06 19:32:03 -0700

    Add openGL build (#5)
    * Add openGL build
    
    * Update
    
    * Fixes
    
    * Update
    
    * Test
    
    * Bump
    
    * Bump
    
    * Bump

---
+++ src/Assets/Scripts/Editor/BuildWebGL.cs
@@ -0,0 +1,16 @@
+﻿//place this script in the Editor folder within Assets.
+using UnityEditor;
+using UnityEngine;
+
+
+//to be used on the command line:
+//$ Unity -quit -batchmode -executeMethod WebGLBuilder.build
+
+public class WebGLBuilder : MonoBehaviour
+{
+    public static void build()
+    {
+        string[] scenes = { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/Level01.unity" };
+        BuildPipeline.BuildPlayer(scenes, "WebGL", BuildTarget.WebGL, BuildOptions.None);
+    }
+}
