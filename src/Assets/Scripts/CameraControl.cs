dc7f90d6b2aae28d6486b5b3c57d320a759bac6e
Erin Alltop <erin.alltop@gmail.com>
2018-07-14 17:14:18 -0400

    Camera fix

--- src/Assets/Scripts/CameraControl.cs
+++ src/Assets/Scripts/CameraControl.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
 	public GameObject player;
+	public bool lockY = true;
 	private Vector3 offset;
+	private Vector3 tempVect;
 
-	// Use this for initialization
-	void Start () {
-		offset = transform.position - player.transform.position;
+	void Start ()
+	{
+		offset = transform.position - player.transform.position; // initial camera offest
+
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
-		transform.position = player.transform.position + offset; // move camera with player position
+
+	void LateUpdate ()
+	{
+		tempVect = player.transform.position + offset;
+		if(lockY) 
+			tempVect.y -= player.transform.position.y + 4.7f; // remove y component of player position
+		transform.position = tempVect;
 	}
+
 }
