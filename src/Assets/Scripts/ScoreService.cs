0d58aea5f79654ce8bf2dcb5ccbe1c5f12fa0c1b
Nathan Sosnovske <nsosnov@microsoft.com>
2018-08-04 14:03:02 -0700

    Update with high score scene

---
+++ src/Assets/Scripts/ScoreService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreService
+{
+    public string Scores { get; set; }
+
+    public IEnumerator FetchScores()
+    {
+        using (WWW www = new WWW("https://cs-467-scores.azurewebsites.net/api/scores"))
+        {
+            while(!www.isDone)
+            {
+                yield return null;
+            }
+
+            Scores = www.text;
+        }
+    }
+}
