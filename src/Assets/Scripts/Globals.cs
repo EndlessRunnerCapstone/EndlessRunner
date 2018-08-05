30718b86e394924a2c7eec0031912b7842a03b15
Nathan Sosnovske <nsosnov@microsoft.com>
2018-08-05 10:41:22 -0700

    Add global player state

--- src/Assets/Scripts/Globals.cs
+++ src/Assets/Scripts/Globals.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Globals {
+public static class Globals
+{
+    static Globals()
+    {
+        PlayerOne = new PlayerState { Score = 0, NumberOfLives = 3 };
+
+        PlayerTwo = new PlayerState { Score = 0, NumberOfLives = 3 };
+    }
 
 	public static bool TwoPlayer { get; set; }
+
+    public static PlayerState PlayerOne { get; set; }
+
+    public static PlayerState PlayerTwo { get; set; }
 }
