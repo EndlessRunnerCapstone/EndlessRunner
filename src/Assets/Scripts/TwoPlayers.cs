c542c08d5c3be950b46ae43b6b3b1a9adbb4dd74
Nathan Sosnovske <nsosnov@microsoft.com>
2018-08-11 13:11:50 -0700

    Fixes

--- src/Assets/Scripts/TwoPlayers.cs
+++ src/Assets/Scripts/TwoPlayers.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class YesButton : MonoBehaviour
+public class TwoPlayers : MonoBehaviour
 {
 
     public void Start()
