2f00669893dcba7abdd5bd8d01ba78818f8fec70
Nathan Sosnovske <nsosnov@microsoft.com>
2018-08-12 13:27:05 -0700

    Add game over to build script

--- src/Assets/Scripts/Editor/BuildWebGL.cs
+++ src/Assets/Scripts/Editor/BuildWebGL.cs
@@ -12,7 +12,7 @@ public class WebGLBuilder : MonoBehaviour
     {
         string[] scenes = { "Assets/Scenes/MainMenu.unity", "Assets/Scenes/WaitingForPlayer.unity", "Assets/Scenes/Level01.unity",
             "Assets/Scenes/Level02.unity", "Assets/Scenes/Level03.unity", "Assets/Scenes/Level04.unity", "Assets/Scenes/LoadingScreen1.unity",
-        "Assets/Scenes/LoadingScreen2.unity", "Assets/Scenes/LoadingScreen3.unity", "Assets/Scenes/LoadingScreen4.unity", "Assets/Scenes/HighScores.unity" };
+        "Assets/Scenes/LoadingScreen2.unity", "Assets/Scenes/LoadingScreen3.unity", "Assets/Scenes/LoadingScreen4.unity", "Assets/Scenes/HighScores.unity", "Assets/Scenes/GameOver.unity" };
         BuildPipeline.BuildPlayer(scenes, "WebGL", BuildTarget.WebGL, BuildOptions.None);
     }
 }
